ed24ef046a76938345f9f042b421d335ee748cae
Alex Ilchenko <ijkilchenko@gmail.com>
2016-08-27 13:45:24 -0400

    Added Kmeans++ and debugged MCMC.

---
+++ kmeans++.py
@@ -0,0 +1,77 @@
+import numpy as np
+import pandas as pd
+from collections import defaultdict
+from bokeh.charts import Scatter, show
+
+
+def dist(x, y):
+    """Euclidean distance between x and y. """
+
+    return np.linalg.norm(x-y)
+
+
+def init_centroids(X, k):
+    centroids = [X[np.random.randint(0, len(X)-1)]]  # First random point.
+    for i in range(1, k):  # For each remaining centroid.
+        D = assign_to_clusters(X, centroids)
+        # Choose new centroid point (from X) according to weights in D.
+        D = [d[0] for d in D]
+        D_sum = sum(D)
+        D = [d/D_sum for d in D]
+        centroids.append(X[np.random.choice(range(len(X)), p=D)])
+    return centroids
+
+
+def assign_to_clusters(X, centroids):
+    """For each data point, calculate the distance to the closest
+    centroid, and put that distance into D. """
+    D = []
+    for j, x in enumerate(X):
+        for m, centroid in enumerate(centroids):
+            curr_dist = dist(centroid, x)
+            if len(D) - 1 < j:
+                D.append((curr_dist, m))
+            else:
+                if D[-1][0] > curr_dist:
+                    D[-1] = (curr_dist, m)
+    return D
+
+
+def recalc_centroids(X, D):
+    cluster_to_indices = defaultdict(lambda: [])
+    for i, d in enumerate(D):
+        cluster_to_indices[d[1]].append(i)
+
+    centroids = []
+    for cluster in cluster_to_indices:
+        new_cluster = [0]*len(X[0])
+        for x in cluster_to_indices[cluster]:
+            x = X[x]
+            for i, x_i in enumerate(x):
+                new_cluster[i] += x_i
+        new_cluster = [c/len(cluster_to_indices[cluster]) for c in new_cluster]
+        centroids.append(new_cluster)
+    return centroids
+
+
+def kmeanspp(X, k, num_iter=100):
+    centroids = init_centroids(X, k)
+
+    for _ in range(num_iter):
+        D = assign_to_clusters(X, centroids)
+        centroids = recalc_centroids(X, D)
+
+    df = pd.DataFrame(X)
+    D = [d[1] for d in D]
+    df = pd.concat([df, pd.Series(D)], axis=1)
+    df.columns = ['x', 'y', 'cluster']
+
+    p = Scatter(df, x='x', y='y', color='cluster')
+
+    show(p)
+
+if __name__ == '__main__':
+    X = [np.array([np.random.uniform(0, 100), np.random.uniform(0, 100)])
+         for _ in range(500)]
+
+    clusters = kmeanspp(X, k=5)
--- mcmc.py
+++ mcmc.py
@@ -1,54 +1,66 @@
-from collections import defaultdict
-from numpy.random import RandomState
 import numpy as np
+from collections import defaultdict
 
-num_points = 4
-
-def _f():
-    return [89, 77, 84, 30]
 
 def f(x):
     """This is the function f(x) which is proportional to some P(x). 
 
-    In this example, consider 4 points in a circle as our space. We 
-    do not know the actual probablity distribution, but we do know 
+    In this example, consider 5 points in a circle as our space. We
+    do not know the actual probability distribution, but we do know
     some relative probabilities. """
 
     return _f()[x]
 
-def metropolis(f=f):
-    x = np.random.randint(num_points)
 
-    def next_x_given_x(x):
-        """This gives the next point located uniformly around x. 
-        This is the jumping distribition. """
-        half_width = 1
-        next_x = np.random.randint(x-half_width, x+half_width+1) # half open [)
-        # loop around since our points are in a circle 
-        return next_x % num_points # note: this works with negative numbers too
+def next_x_given_x(x, width=2):
+    """This gives the next point located normally (rounded to nearest integer)
+    around x. This is the jumping distribution. """
+    num_points = len(_f())
 
-    bins_of_samples = defaultdict(lambda : 0)
-    num_samples = 0
+    next_x = round(np.random.normal()*width)
 
-    num_iter = 100000
+    # Loop around since our points are in a circle.
+    # Note: this works with negative numbers too.
+    return next_x % num_points
+
+
+def metropolis(fun=f, num_iter=10000):
+    """fun is the function f(x) which is proportional to some P(x) which we
+    are trying to approximate. """
+
+    num_points = len(_f())
+    x = np.random.randint(num_points)  # Starting point.
+
+    # We count how many times we observe each point in the Markov sequence.
+    bins_of_samples = defaultdict(lambda: 0)
+
+    # Could be less than num_iter because we reject some samples.
     for i in range(num_iter):
         next_x = next_x_given_x(x)
-        acceptance_ratio = f(next_x)/f(x) # = P(x)/P(next_x)
+        acceptance_ratio = fun(next_x)/fun(x)  # = P(next_x)/P(x).
         if acceptance_ratio >= 1 or np.random.uniform() < acceptance_ratio:
-            # accept next_x 
+            # Accept next_x.
             x = next_x
-            bins_of_samples[x] += 1 # count this sample
-            num_samples += 1
         else:
-            # reject next_x
+            # Reject next_x.
             pass
-    stationary_distribution = [bins_of_samples[p]/num_samples for p in sorted(bins_of_samples)]
+        bins_of_samples[x] += 1  # Count this sample.
+    stationary_distribution = [bins_of_samples[p]/num_iter
+                               for p in bins_of_samples]
     return stationary_distribution
 
-print('target distribution:')
-denominator_P = sum(_f())
-print([float('%.2f'%round(x/denominator_P, 2)) for x in _f()])
+if __name__ == '__main__':
+    for D in [[89, 77, 84, 1, 30],
+              [89, 77, 84, 1, 300]]:
 
-print('stationary distribution:')
-print([round(x, 2) for x in metropolis()])
+        def _f():
+            return D
 
+        print('Target distribution: ')
+        denominator_P = sum(_f())
+        print([float('%.2f' % round(x/denominator_P, 2)) for x in _f()])
+
+        print('Stationary distribution: ')
+        print([round(x, 2) for x in metropolis()])
+
+        print()
